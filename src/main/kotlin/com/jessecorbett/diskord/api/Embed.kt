1cf33710a9575a42616920e592343b0377ca2c8a
Jesse Corbett <jesselcorbett@gmail.com>
2018-07-19 02:06:22 -0500

    Update embed structure

--- src/main/kotlin/com/jessecorbett/diskord/api/Embed.kt
+++ src/main/kotlin/com/jessecorbett/diskord/api/Embed.kt
@@ -4,32 +4,32 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.Instant
 
 data class Embed(
-        @JsonProperty("title") val title: String?,
-        @JsonProperty("type") val type: String,
-        @JsonProperty("description") val description: String?,
-        @JsonProperty("url") val url: String?,
-        @JsonProperty("timestamp") val timestamp: Instant?,
-        @JsonProperty("color") val color: Int,
-        @JsonProperty("footer") val footer: EmbedFooter?,
-        @JsonProperty("image") val image: EmbedImage?,
-        @JsonProperty("thumbnail") val thumbnail: EmbedImage?,
-        @JsonProperty("video") val video: EmbedVideo?,
-        @JsonProperty("provider") val provider: EmbedProvider?,
-        @JsonProperty("author") val author: EmbedAuthor?,
-        @JsonProperty("fields") val fields: Array<EmbedField> = emptyArray()
+        @JsonProperty("title") val title: String? = null,
+        @JsonProperty("description") val description: String? = null,
+        @JsonProperty("url") val url: String? = null,
+        @JsonProperty("timestamp") val timestamp: Instant? = null,
+        @JsonProperty("color") val color: Int? = null,
+        @JsonProperty("footer") val footer: EmbedFooter? = null,
+        @JsonProperty("image") val image: EmbedImage? = null,
+        @JsonProperty("thumbnail") val thumbnail: EmbedImage? = null,
+        @JsonProperty("video") val video: EmbedVideo? = null,
+        @JsonProperty("provider") val provider: EmbedProvider? = null,
+        @JsonProperty("author") val author: EmbedAuthor? = null,
+        @JsonProperty("fields") val fields: Array<EmbedField> = emptyArray(),
+        @JsonProperty("type") val type: String = "rich"
 )
 
 data class EmbedImage(
         @JsonProperty("url") val url: String,
-        @JsonProperty("proxy_url") val proxyUrl: String,
-        @JsonProperty("height") val imageHeight: Int,
-        @JsonProperty("width") val imageWidth: Int
+        @JsonProperty("proxy_url") val proxyUrl: String? = null,
+        @JsonProperty("height") val imageHeight: Int = 0,
+        @JsonProperty("width") val imageWidth: Int = 0
 )
 
 data class EmbedVideo(
         @JsonProperty("url") val url: String,
-        @JsonProperty("height") val videoHeight: Int,
-        @JsonProperty("width") val videoWidth: Int
+        @JsonProperty("height") val videoHeight: Int = 0,
+        @JsonProperty("width") val videoWidth: Int = 0
 )
 
 data class EmbedProvider(
@@ -39,15 +39,15 @@ data class EmbedProvider(
 
 data class EmbedAuthor(
         @JsonProperty("name") val name: String,
-        @JsonProperty("url") val authorUrl: String?,
-        @JsonProperty("icon_url") val authorImageUrl: String?,
-        @JsonProperty("proxy_icon_url") val authorImageProxyUrl: String?
+        @JsonProperty("url") val authorUrl: String? = null,
+        @JsonProperty("icon_url") val authorImageUrl: String? = null,
+        @JsonProperty("proxy_icon_url") val authorImageProxyUrl: String? = null
 )
 
 data class EmbedFooter(
         @JsonProperty("text") val text: String,
-        @JsonProperty("icon_url") val iconUrl: String?,
-        @JsonProperty("proxy_icon_url") val iconProxyUrl: String?
+        @JsonProperty("icon_url") val iconUrl: String? = null,
+        @JsonProperty("proxy_icon_url") val iconProxyUrl: String? = null
 )
 
 data class EmbedField(
