7fdaccd344fab22e4afc0a93ec26de21150d05d1
Jesse Corbett <jesselcorbett@gmail.com>
2018-12-28 23:28:09 -0600

    Add new info to bot gateway endpoint

--- src/main/kotlin/com/jessecorbett/diskord/api/websocket/model/GatewayUrl.kt
+++ src/main/kotlin/com/jessecorbett/diskord/api/websocket/model/GatewayUrl.kt
@@ -1,5 +1,13 @@
 package com.jessecorbett.diskord.api.websocket.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class GatewayUrl(val url: String)
 
-data class GatewayBotUrl(val url: String, val shards: Int)
+data class GatewayBotUrl(val url: String, val shards: Int, @JsonProperty("session_start_limit") val sessionStartLimit: SessionStartLimit)
+
+data class SessionStartLimit(
+        @JsonProperty("total") val totalSessions: Int,
+        @JsonProperty("remaining") val remainingSessions: Int,
+        @JsonProperty("reset_after") val sessionLimitResetsIn: Int
+)
