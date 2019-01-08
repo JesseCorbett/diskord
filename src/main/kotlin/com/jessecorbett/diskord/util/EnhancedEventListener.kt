c238a79f753ea060609df09ea97d54f7a7f0cb1c
Jesse Corbett <jesse.corbett@springventuregroup.com>
2019-01-08 14:24:49 -0600

    Slight formatting clean up

--- src/main/kotlin/com/jessecorbett/diskord/util/EnhancedEventListener.kt
+++ src/main/kotlin/com/jessecorbett/diskord/util/EnhancedEventListener.kt
@@ -10,11 +10,11 @@ import kotlinx.coroutines.runBlocking
 abstract class EnhancedEventListener(token: String) : EventListener() {
     val clientStore = ClientStore(token)
 
-    suspend fun Message.reply(text: String) = clientStore.channels[this.channelId].sendMessage(text)
-    suspend fun Message.delete() = clientStore.channels[this.channelId].deleteMessage(this.id)
+    suspend fun Message.reply(text: String) = clientStore.channels[channelId].sendMessage(text)
+    suspend fun Message.delete() = clientStore.channels[channelId].deleteMessage(id)
 
-    suspend fun MessageUpdate.reply(text: String) = clientStore.channels[this.channelId].sendMessage(text)
-    suspend fun MessageUpdate.delete() = clientStore.channels[this.channelId].deleteMessage(this.id)
+    suspend fun MessageUpdate.reply(text: String) = clientStore.channels[channelId].sendMessage(text)
+    suspend fun MessageUpdate.delete() = clientStore.channels[channelId].deleteMessage(id)
 
     val Message.channel: Channel
         get() = runBlocking { clientStore.channels[channelId].get() }
