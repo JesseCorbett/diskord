06487c72d25e0bea08e046c94410e2af2f2455b9
Jesse Corbett <jesselcorbett@gmail.com>
2019-01-07 21:35:05 -0600

    Simplify command structure

--- src/main/kotlin/com/jessecorbett/diskord/dsl/Commands.kt
+++ src/main/kotlin/com/jessecorbett/diskord/dsl/Commands.kt
@@ -10,7 +10,7 @@ fun Bot.commands(prefix: Char = '.', commands: MutableList<Command> = ArrayList(
     messageCreated { message ->
         if (!message.content.startsWith(prefix)) return@messageCreated
         commands.filter { it.command == message.words[0].drop(1) }.forEach {
-            it.block(it, message)
+            it.action(message)
         }
     }
 }
@@ -19,9 +19,26 @@ fun Bot.commands(prefix: Char = '.', commands: MutableList<Command> = ArrayList(
 class CommandSet(val commands: MutableList<Command>)
 
 @DiskordDsl
-fun CommandSet.command(command: String, action: suspend Command.(Message) -> Unit) {
+fun CommandSet.command(command: String, action: suspend (Message) -> Unit) {
     commands += Command(command, action)
 }
 
 @DiskordDsl
-class Command(val command: String, val block: suspend Command.(Message) -> Unit)
+class Command(val command: String, val action: suspend (Message) -> Unit)
+
+fun main() {
+    bot("MzQ2NDQ0NjE1ODMxNzgxMzc2.DxWZWQ.cEAHWDZlun4H2Fj2fSt0-mlEcf8") {
+        commands {
+            command("ping") { message ->
+                message.delete()
+                println("hewwo")
+                message.reply("pong")
+            }
+
+            command("shutdown") {
+                it.delete()
+                shutdown(true)
+            }
+        }
+    }
+}
