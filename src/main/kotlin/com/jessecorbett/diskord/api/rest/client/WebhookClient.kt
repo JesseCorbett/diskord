0bc08795d3ed15eb7e88fb30edc9d4dc53c992b8
Jesse Corbett <jesse.corbett@springventuregroup.com>
2019-01-10 17:02:03 -0600

    Add webhook client documentation

--- src/main/kotlin/com/jessecorbett/diskord/api/rest/client/WebhookClient.kt
+++ src/main/kotlin/com/jessecorbett/diskord/api/rest/client/WebhookClient.kt
@@ -15,18 +15,75 @@ import com.jessecorbett.diskord.internal.bodyAs
  * @param userType The user type, assumed to be a bot.
  */
 class WebhookClient(token: String, val webhookId: String, userType: DiscordUserType = DiscordUserType.BOT) : RestClient(token, userType) {
+
+    /**
+     * Get this webhook.
+     *
+     * @return This webhook.
+     * @throws DiscordException
+     */
     suspend fun getWebhook() = getRequest("/webhooks/$webhookId").bodyAs<Webhook>()
 
+    /**
+     * Get this webhook using the secure token.
+     *
+     * Does not require authentication and does not include the [User].
+     *
+     * @param webhookToken The webhook's secure token.
+     *
+     * @return This webhook, minus the user.
+     * @throws DiscordException
+     */
     suspend fun getWebhook(webhookToken: String) = getRequest("/webhooks/$webhookId/$webhookToken").bodyAs<Webhook>()
 
+    /**
+     * Update this webhook.
+     *
+     * @param webhook The patched webhook.
+     *
+     * @return The updated webhook.
+     * @throws DiscordException
+     */
     suspend fun update(webhook: PatchWebhook) = patchRequest("/webhooks/$webhookId", webhook).bodyAs<Webhook>()
 
+    /**
+     * Update this webhook using the secure token.
+     *
+     * Does not require authentication and does not include the [User].
+     *
+     * @param webhook The patched webhook.
+     *
+     * @return The updated webhook, minus the user.
+     * @throws DiscordException
+     */
     suspend fun update(webhook: PatchWebhook, webhookToken: String) = patchRequest("/webhooks/$webhookId/$webhookToken", webhook).bodyAs<Webhook>()
 
+    /**
+     * Delete this webhook.
+     *
+     * @throws DiscordException
+     */
     suspend fun delete() = deleteRequest("/webhooks/$webhookId").close()
 
+    /**
+     * Delete this webhook using the secure token.
+     *
+     * Does not require authentication.
+     *
+     * @throws DiscordException
+     */
     suspend fun delete(webhookToken: String) = deleteRequest("/webhooks/$webhookId/$webhookToken").close()
 
+
+    /**
+     * Execute the webhook.
+     *
+     * @param webhookToken The webhook's secure token.
+     * @param webhookSubmission The post the webhook will make.
+     * @param waitForValidation Wait for the message to be posted before the call returns. Defaults to false.
+     *
+     * @throws DiscordException
+     */
     suspend fun execute(webhookToken: String, webhookSubmission: WebhookSubmission, waitForValidation: Boolean = false) =
             postRequest("/webhooks/$webhookId/$webhookToken?wait=$waitForValidation", webhookSubmission).close()
 }
