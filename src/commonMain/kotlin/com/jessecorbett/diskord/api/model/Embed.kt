e3c8431d2993f04bb26d72d3241c9a8bc8d40781
Jesse Corbett <jesse.corbett@springventuregroup.com>
2019-02-13 13:29:05 -0600

    Update embed to respect optional fields

--- src/commonMain/kotlin/com/jessecorbett/diskord/api/model/Embed.kt
+++ src/commonMain/kotlin/com/jessecorbett/diskord/api/model/Embed.kt
@@ -1,29 +1,30 @@
 package com.jessecorbett.diskord.api.model
 
+import kotlinx.serialization.Optional
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Embed(
-        @SerialName("title") val title: String? = null,
-        @SerialName("description") val description: String? = null,
-        @SerialName("url") val url: String? = null,
-        @SerialName("timestamp") val timestamp: String? = null,
-        @SerialName("color") val color: Color? = null,
-        @SerialName("footer") val footer: EmbedFooter? = null,
-        @SerialName("image") val image: EmbedImage? = null,
-        @SerialName("thumbnail") val thumbnail: EmbedImage? = null,
-        @SerialName("video") val video: EmbedVideo? = null,
-        @SerialName("provider") val provider: EmbedProvider? = null,
-        @SerialName("author") val author: EmbedAuthor? = null,
-        @SerialName("fields") val fields: List<EmbedField> = emptyList(),
+        @Optional @SerialName("title") val title: String? = null,
+        @Optional @SerialName("description") val description: String? = null,
+        @Optional @SerialName("url") val url: String? = null,
+        @Optional @SerialName("timestamp") val timestamp: String? = null,
+        @Optional @SerialName("color") val color: Color? = null,
+        @Optional @SerialName("footer") val footer: EmbedFooter? = null,
+        @Optional @SerialName("image") val image: EmbedImage? = null,
+        @Optional @SerialName("thumbnail") val thumbnail: EmbedImage? = null,
+        @Optional @SerialName("video") val video: EmbedVideo? = null,
+        @Optional @SerialName("provider") val provider: EmbedProvider? = null,
+        @Optional @SerialName("author") val author: EmbedAuthor? = null,
+        @Optional @SerialName("fields") val fields: List<EmbedField> = emptyList(),
         @SerialName("type") val type: String = "rich"
 )
 
 @Serializable
 data class EmbedImage(
         @SerialName("url") val url: String,
-        @SerialName("proxy_url") val proxyUrl: String? = null,
+        @Optional @SerialName("proxy_url") val proxyUrl: String? = null,
         @SerialName("height") val imageHeight: Int = 0,
         @SerialName("width") val imageWidth: Int = 0
 )
@@ -44,16 +45,16 @@ data class EmbedProvider(
 @Serializable
 data class EmbedAuthor(
         @SerialName("name") val name: String,
-        @SerialName("url") val authorUrl: String? = null,
-        @SerialName("icon_url") val authorImageUrl: String? = null,
-        @SerialName("proxy_icon_url") val authorImageProxyUrl: String? = null
+        @Optional @SerialName("url") val authorUrl: String? = null,
+        @Optional @SerialName("icon_url") val authorImageUrl: String? = null,
+        @Optional @SerialName("proxy_icon_url") val authorImageProxyUrl: String? = null
 )
 
 @Serializable
 data class EmbedFooter(
         @SerialName("text") val text: String,
-        @SerialName("icon_url") val iconUrl: String? = null,
-        @SerialName("proxy_icon_url") val iconProxyUrl: String? = null
+        @Optional @SerialName("icon_url") val iconUrl: String? = null,
+        @Optional @SerialName("proxy_icon_url") val iconProxyUrl: String? = null
 )
 
 @Serializable
