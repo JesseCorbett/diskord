999f780cfba48f666aa6360408f6daa825def4b1
Matthew Gast <incendium@gmail.com>
2021-03-29 19:49:10 -0500

    No need to mutate user flags (only get from API, no set)

--- diskord-core/src/commonMain/kotlin/com/jessecorbett/diskord/api/common/UserFlag.kt
+++ diskord-core/src/commonMain/kotlin/com/jessecorbett/diskord/api/common/UserFlag.kt
@@ -9,19 +9,19 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
 public enum class UserFlag(internal val mask: Int) {
-    DISCORD_EMPLOYEE(1 shl 0),
-    PARTNERED_SERVER_OWNER(1 shl 1),
-    HYPESQUAD_EVENTS(1 shl 2),
-    BUG_HUNTER_LEVEL_1(1 shl 3),
-    HOUSE_BRAVERY(1 shl 6),
-    HOUSE_BRILLIANCE(1 shl 7),
-    HOUSE_BALANCE(1 shl 8),
-    EARLY_SUPPORTER(1 shl 9),
-    TEAM_USER(1 shl 10),
-    SYSTEM(1 shl 12),
-    BUG_HUNTER_LEVEL_2(1 shl 14),
-    VERIFIED_BOT(1 shl 16),
-    EARLY_VERIFIED_BOT_DEVELOPER(1 shl 17);
+    DISCORD_EMPLOYEE(0x00000001),
+    PARTNERED_SERVER_OWNER(0x00000002),
+    HYPESQUAD_EVENTS(0x00000004),
+    BUG_HUNTER_LEVEL_1(0x00000008),
+    HOUSE_BRAVERY(0x00000040),
+    HOUSE_BRILLIANCE(0x00000080),
+    HOUSE_BALANCE(0x00000100),
+    EARLY_SUPPORTER(0x00000200),
+    TEAM_USER(0x00000400),
+    SYSTEM(0x00001000),
+    BUG_HUNTER_LEVEL_2(0x00004000),
+    VERIFIED_BOT(0x00010000),
+    EARLY_VERIFIED_BOT_DEVELOPER(0x00020000);
 }
 
 /**
@@ -34,8 +34,6 @@ public enum class UserFlag(internal val mask: Int) {
  *         // do something
  *     }
  * ```
- *
- * *Note: All operations on a [UserFlags] instance will create a new [UserFlags] instance.*
  */
 @Serializable(with = UserFlagsSerializer::class)
 public data class UserFlags(val value: Int) {
@@ -47,32 +45,10 @@ public data class UserFlags(val value: Int) {
         return value and flags.value == flags.value
     }
 
-    public operator fun plus(flags: Int): UserFlags = UserFlags(value or flags)
-
-    public operator fun plus(flags: UserFlags): UserFlags = plus(flags.value)
-
-    public operator fun plus(flags: Collection<UserFlag>): Unit = flags.forEach { plus(it.mask) }
-
-    public operator fun plus(flag: UserFlag): UserFlags = plus(flag.mask)
-
-    public operator fun minus(flags: Int): UserFlags = UserFlags(value and flags.inv())
-
-    public operator fun minus(flags: UserFlags): UserFlags = minus(flags.value)
-
-    public operator fun minus(flags: Collection<UserFlag>): Unit = flags.forEach { minus(it.mask) }
-
-    public operator fun minus(flag: UserFlag): UserFlags = minus(flag.mask)
-
     override fun toString(): String = "UserFlags($value) --> ${UserFlag.values().filter { it in value }.joinToString()}"
 
     public companion object {
-        public val ALL: UserFlags = of(*UserFlag.values())
-
         public val NONE: UserFlags = UserFlags(0)
-
-        public fun of(vararg flags: UserFlag): UserFlags {
-            return UserFlags(flags.map { flag -> flag.mask }.reduce { left, right -> left or right })
-        }
 
         private operator fun Int.contains(flag: UserFlag) = this and flag.mask == flag.mask
     }
--- diskord-core/src/jvmTest/kotlin/com/jessecorbett/diskord/api/common/UserFlagsTest.kt
+++ diskord-core/src/jvmTest/kotlin/com/jessecorbett/diskord/api/common/UserFlagsTest.kt
@@ -1,18 +1,36 @@
 package com.jessecorbett.diskord.api.common
 
 import assertk.assertThat
+import assertk.assertions.isEqualTo
 import assertk.assertions.isFalse
 import assertk.assertions.isTrue
 import kotlin.test.Test
 
 internal class UserFlagsTest {
     @Test
-    fun `should handle detecting a flag in flags`() {
-        val flags = UserFlags.of(UserFlag.HYPESQUAD_EVENTS, UserFlag.HOUSE_BRAVERY)
+    fun `should be equal to none if empty`() {
+        val flags = UserFlags(0)
+
+        assertThat(flags).isEqualTo(UserFlags.NONE)
+    }
+
+    @Test
+    fun `should handle detecting a flag in flags (our values)`() {
+        val flags = UserFlags(UserFlag.HYPESQUAD_EVENTS.mask + UserFlag.HOUSE_BRAVERY.mask)
 
         assertThat(UserFlag.HYPESQUAD_EVENTS in flags).isTrue()
         assertThat(UserFlag.HOUSE_BRAVERY in flags).isTrue()
         assertThat(UserFlag.HOUSE_BALANCE in flags).isFalse()
         assertThat(UserFlag.HOUSE_BRILLIANCE in flags).isFalse()
+    }
+
+    @Test
+    fun `should handle detecting a flag in flags (real values)`() {
+        val flags = UserFlags(640) // HOUSE_BRILLIANCE, EARLY_SUPPORTER
+
+        assertThat(UserFlag.EARLY_SUPPORTER in flags).isTrue()
+        assertThat(UserFlag.HOUSE_BRILLIANCE in flags).isTrue()
+        assertThat(UserFlag.HOUSE_BRAVERY in flags).isFalse()
+        assertThat(UserFlag.HOUSE_BALANCE in flags).isFalse()
     }
 }
