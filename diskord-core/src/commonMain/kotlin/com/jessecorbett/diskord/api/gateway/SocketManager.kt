511714d1f8f4197b5025ff685a32232135f5041e
Jesse Corbett <jesselcorbett@gmail.com>
2021-04-02 00:59:55 -0500

    Move socket connection coroutine context into the try-catch handling restart/close logic instead of outside it

--- diskord-core/src/commonMain/kotlin/com/jessecorbett/diskord/api/gateway/SocketManager.kt
+++ diskord-core/src/commonMain/kotlin/com/jessecorbett/diskord/api/gateway/SocketManager.kt
@@ -45,60 +45,62 @@ internal class SocketManager(url: String, private val emitMessage: suspend (Gate
 
     suspend fun close() {
         running = false
-        session?.close(CloseReason(WebSocketCloseCode.NORMAL_CLOSURE.code, "This bot requested the connection close"))
+        session?.close(CloseReason(WebSocketCloseCode.NORMAL_CLOSURE.code, "The bot requested the connection close"))
         socketClient.close()
     }
 
     suspend fun restartConnection() {
-        session?.close(CloseReason(WebSocketCloseCode.NORMAL_CLOSURE.code, "This bot requested the connection close to restart"))
+        session?.close(CloseReason(WebSocketCloseCode.NORMAL_CLOSURE.code, "The bot requested the connection close in order to restart"))
     }
 
     suspend fun send(gatewayMessage: GatewayMessage) {
         outgoingMessages.send(gatewayMessage)
     }
 
-    private suspend fun initializeNewConnection() = coroutineScope {
-        try {
-            socketClient.wss(host = url, port = 443, request = {
-                this.url.parameters["v"] = "8"
-                this.url.parameters["encoding"] = "json"
-                logger.trace { "Building a socket HttpRequest" }
-            }) {
-                logger.info { "Starting a new websocket connection" }
+    private suspend fun initializeNewConnection() = try {
+            coroutineScope {
+                socketClient.wss(host = url, port = 443, request = {
+                    this.url.parameters["v"] = "8"
+                    this.url.parameters["encoding"] = "json"
+                    logger.trace { "Building a socket HttpRequest" }
+                }) {
+                    logger.info { "Starting a new websocket connection" }
 
-                session = this
+                    session = this
 
-                logger.info { "Starting incoming loop" }
+                    logger.info { "Starting incoming loop" }
 
-                launch {
-                    for (message in outgoingMessages) {
-                        send(defaultJson.encodeToString(message))
+                    launch {
+                        for (message in outgoingMessages) {
+                            send(defaultJson.encodeToString(message))
+                        }
                     }
-                }
 
-                for (frame in incoming) {
-                    logger.trace { "Incoming Message:\nInfo:\n\tframe = $frame\nData:\n${frame.data.toHexDump(rowPrefix = "\t")}" }
+                    for (frame in incoming) {
+                        logger.trace { "Incoming Message:\nInfo:\n\tframe = $frame\nData:\n${frame.data.toHexDump(rowPrefix = "\t")}" }
 
-                    when (frame) {
-                        is Frame.Text -> emitMessage(defaultJson.decodeFromString(GatewayMessage.serializer(), frame.readText()))
-                        is Frame.Binary -> TODO("Add support for binary formatted data")
-                        is Frame.Close -> logger.info { "Close Frame sent with message: $frame" }
-                        is Frame.Ping, is Frame.Pong -> logger.debug { frame }
+                        when (frame) {
+                            is Frame.Text -> emitMessage(defaultJson.decodeFromString(GatewayMessage.serializer(), frame.readText()))
+                            is Frame.Binary -> TODO("Add support for binary formatted data")
+                            is Frame.Close -> logger.info { "Close Frame sent with message: $frame" }
+                            is Frame.Ping, is Frame.Pong -> logger.debug { frame }
+                        }
                     }
-                }
-                logger.info { "Exited the incoming loop" }
+                    logger.info { "Exited the incoming loop" }
 
-                val closeReason = this@wss.closeReason.await()
-                if (closeReason == null) {
-                    logger.warn { "Closed with no close reason, probably a connection issue" }
-                } else {
-                    val closeCode = WebSocketCloseCode.values().find { it.code == closeReason.code }
-                    val message = if (closeReason.message.isEmpty()) {
-                        "Closed with code '$closeCode' with no reason provided"
+                    val closeReason = this@wss.closeReason.await()
+                    if (closeReason == null) {
+                        logger.warn { "Closed with no close reason, probably a connection issue" }
                     } else {
-                        "Closed with code '$closeCode' with the reason '${closeReason.message}'"
+                        val closeCode = WebSocketCloseCode.values().find { it.code == closeReason.code }
+                        val message = if (closeReason.message.isEmpty()) {
+                            "Closed with code '$closeCode' with no reason provided"
+                        } else {
+                            "Closed with code '$closeCode' with the reason '${closeReason.message}'"
+                        }
+                        logger.info { message }
                     }
-                    logger.info { message }
+                    this@coroutineScope.cancel()
                 }
             }
         } finally {
@@ -109,7 +111,6 @@ internal class SocketManager(url: String, private val emitMessage: suspend (Gate
             }
             session?.cancel()
             session = null
-        }
     }
 
     private fun buildWSClient(): HttpClient {
