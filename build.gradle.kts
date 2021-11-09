8244ca7f01744ca04bf1a7f8b4d9ca5ea01d66b2
Jesse Corbett <jesselcorbett@gmail.com>
2021-11-09 02:11:34 -0600

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     `maven-publish`
     signing
 
-    id("org.jetbrains.kotlin.multiplatform") version "1.5.30" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.5.30" apply false
+    id("org.jetbrains.kotlin.multiplatform") version "1.5.31" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.5.31" apply false
     id("org.jetbrains.dokka") version "1.5.0"
 }
 
