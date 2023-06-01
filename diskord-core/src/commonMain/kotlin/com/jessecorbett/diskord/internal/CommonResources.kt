ff16ee005c75e8ce6e5969a97509ee036ef09855
Jesse Corbett <jesselcorbett@gmail.com>
2023-06-01 10:19:38 -0700

    Prepare for next release

--- diskord-core/src/commonMain/kotlin/com/jessecorbett/diskord/internal/CommonResources.kt
+++ diskord-core/src/commonMain/kotlin/com/jessecorbett/diskord/internal/CommonResources.kt
@@ -2,8 +2,8 @@ package com.jessecorbett.diskord.internal
 
 import io.ktor.client.engine.*
 
-internal const val defaultUserAgentUrl = "https://gitlab.com/jesselcorbett/diskord"
-internal const val defaultUserAgentVersion = "4.1.0"
+internal const val defaultUserAgentUrl = "https://gitlab.com/diskord/diskord"
+internal const val defaultUserAgentVersion = "4.1.1"
 
 internal expect fun websocketClient(): HttpClientEngineFactory<HttpClientEngineConfig>
 internal expect fun httpClient(): HttpClientEngineFactory<HttpClientEngineConfig>
