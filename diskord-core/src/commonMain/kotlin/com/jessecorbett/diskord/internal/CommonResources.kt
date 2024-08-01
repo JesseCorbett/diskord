08b63a54f69f7d5a9d1dd596ee2c97504fa00688
Jesse Corbett <jesselcorbett@gmail.com>
2024-07-31 19:07:59 -0700

    Upgrade to final release versions of deps

--- diskord-core/src/commonMain/kotlin/com/jessecorbett/diskord/internal/CommonResources.kt
+++ diskord-core/src/commonMain/kotlin/com/jessecorbett/diskord/internal/CommonResources.kt
@@ -3,7 +3,7 @@ package com.jessecorbett.diskord.internal
 import io.ktor.client.engine.*
 
 internal const val defaultUserAgentUrl = "https://gitlab.com/diskord/diskord"
-internal const val defaultUserAgentVersion = "5.4.2"
+internal const val defaultUserAgentVersion = "5.5.0"
 
 internal expect fun websocketClient(): HttpClientEngineFactory<HttpClientEngineConfig>
 internal expect fun httpClient(): HttpClientEngineFactory<HttpClientEngineConfig>
